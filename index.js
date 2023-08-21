2b7a75a6d7d838858052902535c498a924b6afd4
TimiMaliki <malikitimi@gmail.com>
2023-08-21 15:33:08 +0100

    left some comments in the js

--- index.js
+++ index.js
@@ -3,46 +3,48 @@ const input = document.getElementById("new-task-input");
 const listEl = document.querySelector("#tasks");
 const newTaskSubmit = document.getElementById("new-task-submit");
 
-//the submit form task eventListener
+//the add task submit form  eventListener
 
 newTaskSubmit.addEventListener("click", (e) => {
   e.preventDefault();
   const task = input.value; // the input.value are the texts you provide in the input form
 
   input.value = "";
-
+ // if user hasnt filled anything alert
   if (!task) {
     alert("fill the form");
     return;
   }
 
   const taskEl = document.createElement("div"); //createElement allows me to create DOM notes
-  taskEl.classList.add("task");
+  // that creates the task form
+  taskEl.classList.add("task"); //added the class of task
 
-  const taskContentEl = document.createElement("div");
-  taskContentEl.classList.add("content");
-  taskContentEl.innerHTML = task;
+  const taskContentEl = document.createElement("div");//createElement allows me to create DOM notes
+  taskContentEl.classList.add("content");//added the class of content
+  taskContentEl.innerHTML = task; // the textContent should be equall to inputvalue
 
-  taskEl.appendChild(taskContentEl);
+  taskEl.appendChild(taskContentEl); //add the taskContent inside the created div
 
-  const actionEl = document.createElement("div");
-  actionEl.classList.add("btn-actions");
+  const actionEl = document.createElement("div");//createElement allows me to create DOM notes
+  actionEl.classList.add("btn-actions");//added the class of btn-actions
 
-  const deleteEl = document.createElement("button");
-  deleteEl.classList.add("delete");
-  deleteEl.innerHTML = "Delete";
+  const deleteEl = document.createElement("button"); // Dom element
+  deleteEl.classList.add("delete");//added the class of delete
+  deleteEl.innerHTML = "Delete";// textContent should be a string of "Delete"
 
-  actionEl.appendChild(deleteEl);
+  actionEl.appendChild(deleteEl);//add the delete button inside the created div
 
+  //add the action btn and the delete btn inside the div taskEl created
   taskEl.appendChild(actionEl);
   taskEl.appendChild(deleteEl);
-
+ //add the everything inside the form created
   listEl.appendChild(taskEl);
 
 
   taskContentEl
 
-
+//the delete button eventlistener
   deleteEl.addEventListener('click' , (e) =>{
     if(deleteEl !== taskEl){
         listEl.removeChild(taskEl)
