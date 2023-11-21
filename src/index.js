15a5816814c74f007e1b392bd37123470e0692ff
TimiMaliki <malikitimi@gmail.com>
2023-11-21 22:49:06 +0100

    added: delete button

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const addBtn = document.querySelector(".add-btn");
+const addBtn = document.querySelector(".add-button");
 
 import { initializeApp } from "firebase/app";
 
@@ -7,37 +7,36 @@ import {
   collection,
   onSnapshot,
   addDoc,
-  deleteDoc,
-  doc,
 } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA-XoDZYwg7jxz4E3XKIa3pAoVcdD4cg2g",
-  authDomain: "first-a96df.firebaseapp.com",
-  projectId: "first-a96df",
-  storageBucket: "first-a96df.appspot.com",
-  messagingSenderId: "855755296158",
-  appId: "1:855755296158:web:8373ab94f30843000b5a8f",
-  measurementId: "G-6K674LRDDR",
+  apiKey: "AIzaSyAzI6DW9qx6xHLHgDEbh3c5esIfqGZU45M",
+  authDomain: "myapp-17d11.firebaseapp.com",
+  projectId: "myapp-17d11",
+  storageBucket: "myapp-17d11.appspot.com",
+  messagingSenderId: "230722705247",
+  appId: "1:230722705247:web:f02846f08e6409b5efc8ea",
+  measurementId: "G-M1ZFJTFWNP",
 };
 
 initializeApp(firebaseConfig);
-
 const db = getFirestore();
-const colRef = collection(db, "task-todo");
+const colRef = collection(db, "todo");
+
+console.log(colRef);
 
 addBtn.addEventListener("click", (e) => {
   e.preventDefault();
 
   let text = document.querySelector(".input-todo-value").value;
+
   if (!text) {
     alert("Empty");
-
     return;
   }
   document.querySelector(".input-todo-value").value = "";
   addDoc(colRef, {
-    text: text,
+    text,
   });
 });
 
@@ -54,58 +53,36 @@ function getItems() {
 
 function generateItems(tasks) {
   let itemsHtml = "";
-  
 
   tasks.forEach((item) => {
     console.log(item);
 
     itemsHtml += `
-
-   
-    <div class="flex-input data-id">
-   
-       <div class="text">${item.text}</div>
-    </div>
- 
-     `;
-
-  
+    
+    
+        <div class="wrapper">
+    <ul>
+            <li>
+            ${item.text}
+            </li>
+        </ul>
+        </div>
+         `;
   });
 
   document.querySelector(".wrapper").innerHTML = itemsHtml;
 
-  deleteButton(tasks)
+
 }
 
 getItems();
 
 
 
-
-
-
-
-
-
-
-function deleteButton(tasks) {
-
   document.querySelector(".delete").addEventListener("click", (e) => {
     e.preventDefault();
-
-    tasks.forEach((item) => {
-
-
-
-      const deleteRef = doc(db, "task-todo", item.id);
-    
-      deleteDoc(deleteRef);
-    });
-
-    
-
-  });
+    const list = document.querySelector(".wrapper")
+    list.removeChild(list.firstElementChild)
   
-}
-
+  });
 
